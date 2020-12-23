d29bc4baef866b704850ed3208a517e61ab87113
jonghancha <73284068+jonghancha@users.noreply.github.com>
2020-12-23 16:46:32 +0900

    주소록 db

---
+++ AddressBook.sql
@@ -0,0 +1,91 @@
+CREATE DATABASE  IF NOT EXISTS `AddressBook` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `AddressBook`;
+-- MySQL dump 10.13  Distrib 8.0.17, for macos10.14 (x86_64)
+--
+-- Host: localhost    Database: AddressBook
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `address`
+--
+
+DROP TABLE IF EXISTS `address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `address` (
+  `addressNo` int(11) NOT NULL AUTO_INCREMENT,
+  `user_userNo` int(11) NOT NULL,
+  `addressName` varchar(45) DEFAULT NULL,
+  `addressPhone` varchar(45) DEFAULT NULL,
+  `addressGroup` varchar(45) DEFAULT NULL,
+  `addressEmail` varchar(45) DEFAULT NULL,
+  `addressText` varchar(45) DEFAULT NULL,
+  `addressBirth` varchar(45) DEFAULT NULL,
+  `addressImage` varchar(45) DEFAULT NULL,
+  `addressStar` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`addressNo`,`user_userNo`),
+  UNIQUE KEY `addressNo_UNIQUE` (`addressNo`),
+  KEY `fk_address_user1_idx` (`user_userNo`),
+  CONSTRAINT `fk_address_user1` FOREIGN KEY (`user_userNo`) REFERENCES `user` (`userNo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `address`
+--
+
+LOCK TABLES `address` WRITE;
+/*!40000 ALTER TABLE `address` DISABLE KEYS */;
+/*!40000 ALTER TABLE `address` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `userNo` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` varchar(45) DEFAULT NULL,
+  `userPw` varchar(45) DEFAULT NULL,
+  `userName` varchar(45) DEFAULT NULL,
+  `userPhone` varchar(45) DEFAULT NULL,
+  `userEmail` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`userNo`),
+  UNIQUE KEY `userNo_UNIQUE` (`userNo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-23 16:46:01
